91715401e276270112a654ccbec1c303870d1c70
Renan G Molero <molero.renan@kingpage.com.br>
2023-01-11 09:07:05 -0300

    create user on prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [UserModule, PrismaModule],
